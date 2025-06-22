fe0e08794b41eb11f424d08716820663d055461c
João Fontoura <joaofontourab@gmail.com>
2025-06-22 13:22:13 -0300

    implementacao das telas e transicao entre elas

--- jogomain.c
+++ jogomain.c
@@ -1,9 +1,19 @@
 #include <raylib.h>
 #include <stdio.h>
 
+typedef enum GameScreen { MENU = 0, JOGO, LEADERBOARD, FECHAR } GameScreen;
+
+
 int main(void){
     
     InitWindow(800, 600, "Hello Raylib");
+    GameScreen currentScreen = MENU;// Definindo a tela atual como MENU
+
+    int btnState = 0;               // Button state: 0-NORMAL, 1-MOUSE_HOVER, 2-PRESSED
+    bool btnAction = false;         // Button action should be activated
+
+    Vector2 mousePoint = { 0.0f, 0.0f }; 
+
     SetTargetFPS(60);
 
     int screenWidth = 800;
@@ -26,21 +36,101 @@ int main(void){
     UnloadImage(imagebackground); 
     UnloadImage(imagebotaoplay); 
     UnloadImage(imagebotaoleaderboard);
-    UnloadImage(imagebotaosair);// descarregando as imagens, pois ja foram carregadas como texturas.  
-    
-
+    UnloadImage(imagebotaosair); // descarregando as imagens, pois ja foram carregadas como texturas.
+    Rectangle playBounds = {+ 365, + 165, botaoplay.width * 0.2f, botaoplay.height * 0.2f }; // Definindo os limites do botao play
 
     while (WindowShouldClose() == false) {
         
+        // Update de telas
+        switch (currentScreen)
+        {
+            case MENU:
+            {
+                mousePoint = GetMousePosition();
+                btnAction = false;
+
+        // Check button state
+        if (CheckCollisionPointRec(mousePoint, playBounds))
+        {
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) btnState = 2;
+            else btnState = 1;
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnAction = true;
+        }
+        else btnState = 0;
+        if (btnAction){
+            // Change to JOGO screen
+            currentScreen = JOGO;
+        }
+            } break;
+            case JOGO:
+            {
+                // TODO: Update TITLE screen variables here!
+
+                // Press enter to change to GAMEPLAY screen
+           
+            } break;
+            case LEADERBOARD:
+            {
+                // TODO: Update GAMEPLAY screen variables here!
+
+                // Press enter to change to ENDING screen
+               
+            } break;
+            case FECHAR:
+            {
+                // TODO: Update ENDING screen variables here!
+
+                // Press enter to return to TITLE screen
+              
+            } break;
+            default: break;
+        }
 
         // Draw
+        
         BeginDrawing();
         ClearBackground(RAYWHITE);
+         switch(currentScreen)
+            {
+                case MENU:
+                {
+                    DrawTextureEx(background, (Vector2){ x, y }, 0.0f, escalabackground, WHITE);
+                    DrawTextureEx(botaoplay, (Vector2){ x + 365, y + 165 }, 0.0f, 0.2f, WHITE); // desenhando o background e o botao play na tela.
+                    DrawTextureEx(botaoleaderboard, (Vector2){ x + 365, y + 310 }, 0.0f, 0.2f, WHITE); // desenhando o botao leaderboard na tela.
+                    DrawTextureEx(botaosair, (Vector2){ x + 365, y + 455 }, 0.0f, 0.2f, WHITE); // desenhando o botao sair na tela.
+        
 
-        DrawTextureEx(background, (Vector2){ x, y }, 0.0f, escalabackground, WHITE);
-        DrawTextureEx(botaoplay, (Vector2){ x + 365, y + 165 }, 0.0f, 0.2f, WHITE); // desenhando o background e o botao play na tela.
-        DrawTextureEx(botaoleaderboard, (Vector2){ x + 365, y + 310 }, 0.0f, 0.2f, WHITE); // desenhando o botao leaderboard na tela.
-        DrawTextureEx(botaosair, (Vector2){ x + 365, y + 455 }, 0.0f, 0.2f, WHITE); // desenhando o botao sair na tela.
+                } break;
+                case JOGO:
+                {
+                    // TODO: Draw TITLE screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+                    DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+                    DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+
+                } break;
+                case LEADERBOARD:
+                {
+                    // TODO: Draw GAMEPLAY screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                    DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
+                    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+
+                } break;
+                case FECHAR:
+                {
+                    // TODO: Draw ENDING screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                    DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+                    DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+
+                } break;
+                default: break;
+            }
+
+      
+        
         EndDrawing();
     }
     {
